782cec0fecd2231d1a9a6ab3699c1d7ee7cdee1c
launch vbs <>
2021-04-23 01:23:07 +0000

    Little changes and fixing a command.

--- Commands/noobify.js
+++ Commands/noobify.js
@@ -1,32 +1,33 @@
 module.exports = {
-	name: 'noobify',
-	description: 'says if a member is a noob',
-  cooldown: 10,
-	execute(message, args) {
+  name: "noobify",
+  args: true,
+  execute(message, args) {
     const random = (min = 0, max = 50) => {
       let num = Math.random() * (max - min) + min;
 
       return Math.round(num);
     };
-    const breh = require("/home/runner/Launcher/Commands/storages/noobifyStorage.js")
-    let user = message.member.id;
-    let bruh = 0
-    noobOrNot.forEach(checkIfThere, ++bruh)
-    function checkIfThere(item) {
-      if (item == user) {
-        message.channel.send(areYouNoob[bruh] + "test")
+    async function lmao() {
+          const members = await message.guild.members.random();
+    }
+
+    if (!args[0]){
+      const members = message.member
+      console.log(members)
+      const bruh = random(0, 1)
+      if (bruh == "0") {
+        message.channel.send("<@" + members.id + ">" + " was a noob")
       }else{
-        let maybe = random(0, 1);
-        if (maybe === 1){
-          message.channel.send("You are a noob")
-          noobOrNot.push(user)
-          areYouNoob.push("you are a noob >:)")
-        }else{
-          message.channel.send("You are not a noob, good job")
-          noobOrNot.push(user)
-          areYouNoob.push("You are not a noob, pog")
-        }
+        message.channel.send("<@" + members.id + ">" + " was not a nub.")
+      }
+    }else{
+      const user = args[0]
+      const bruh = random(0, 1)
+      if (bruh === "0"){
+        message.channel.send(user+" was a noob.")
+      }else{
+        message.channel.send(user+" was not a noob.")
       }
     }
-    }
-	};
+  }
+};
